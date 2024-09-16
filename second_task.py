294751d2d1bc2bd1889e785ddb372b1a55e27a67
akhudiak <artem.khudiak@gmail.com>
2024-09-16 08:02:29 +0300

    third_task

--- second_task.py
+++ second_task.py
@@ -14,5 +14,5 @@ def get_numbers_ticket(min, max, quantity):
         return []
 
 
-lottery_numbers = get_numbers_ticket(1, 49, 6)
-print("Ваші лотерейні числа:", lottery_numbers)
+# lottery_numbers = get_numbers_ticket(1, 49, 6)
+# print("Ваші лотерейні числа:", lottery_numbers)
