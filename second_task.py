d12e63f9a696774393ae5046fd4c0cea46b8a02a
akhudiak <artem.khudiak@gmail.com>
2024-09-17 16:14:50 +0300

    Second task, condition 3 editing

--- second_task.py
+++ second_task.py
@@ -5,7 +5,7 @@ def get_numbers_ticket(min, max, quantity):
 
     cond1 = min >= 1
     cond2 = max <= 1000
-    cond3 = min <= quantity <= max
+    cond3 = 1 <= quantity <= max - min + 1
 
     if cond1 and cond2 and cond3:
         win_numbers = random.sample(range(min, max + 1), quantity)
@@ -14,5 +14,5 @@ def get_numbers_ticket(min, max, quantity):
         return []
 
 
-# lottery_numbers = get_numbers_ticket(1, 49, 6)
-# print("Ваші лотерейні числа:", lottery_numbers)
+lottery_numbers = get_numbers_ticket(10, 4, 6)
+print("Ваші лотерейні числа:", lottery_numbers)
